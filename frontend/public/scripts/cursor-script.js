75ae37dd5d875d3563eb65b5c9e24bc0e298112a
Joshua Porporino <porporinoj2@montclair.edu>
2023-03-03 22:40:42 -0800

    Fixed Navbar Link Issue

--- frontend/public/scripts/cursor-script.js
+++ frontend/public/scripts/cursor-script.js
@@ -10,6 +10,6 @@ document.addEventListener("mousemove", e => {
 document.addEventListener("click", e => {
   cursor?.classList.add("expand");
   setTimeout(() => {
-    cursor.classList.remove("expand");
+    cursor?.classList.remove("expand");
   }, 150);
 });
