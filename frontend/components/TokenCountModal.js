998a39047b44f3cf30b6dfc8571e49abbc6ea0cc
Uzair Saiyed <67183061+JustUzair@users.noreply.github.com>
2023-03-09 06:29:46 +0530

    Fixed a bug in create token modal

--- frontend/components/TokenCountModal.js
+++ frontend/components/TokenCountModal.js
@@ -44,8 +44,8 @@ const TokenCountModal = ({ tokenCountModal, setTokenCountModal }) => {
         functionName: "issueToken",
         params: {
           _tokenAddress: tokenAddress,
-          //   amount: ethers.utils.parseEther(tokenAmount.toString()).toString(),
-          amount: ethers.utils.parseEther("1"),
+          amount: ethers.utils.parseEther(tokenAmount.toString()).toString(),
+          // amount: ethers.utils.parseEther("1"),
         },
       },
       onError: error => {
